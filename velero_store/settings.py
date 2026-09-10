14872ef7b9f2345e31fda3f728b4ceebfbc83521
Rafael Gonzalez <rafadev12>
2026-09-09 23:43:05 -0400

    setting

--- velero_store/settings.py
+++ velero_store/settings.py
@@ -45,11 +45,12 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Debe ir justo debajo de SecurityMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',  # <-- Requerido por el Admin
+    'django.contrib.messages.middleware.MessageMiddleware',      # <-- Requerido por el Admin
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
