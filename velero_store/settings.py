05460217dd9fed17c0b004c7b1ad0d5bd6a115b5
Rafael Gonzalez <rafadev12>
2026-09-09 23:45:04 -0400

    Limpiar middlewares duplicados

--- velero_store/settings.py
+++ velero_store/settings.py
@@ -45,12 +45,12 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Debe ir justo debajo de SecurityMiddleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',  # <-- Requerido por el Admin
-    'django.contrib.messages.middleware.MessageMiddleware',      # <-- Requerido por el Admin
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
