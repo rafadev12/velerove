cb829ed5c45bd7039802a7607321e4b9940fe67c
Rafael Gonzalez <rafadev12>
2026-09-20 14:38:42 -0400

    Incluir categorias y productos en productos.json

--- exportar.py
+++ exportar.py
@@ -2,11 +2,10 @@ import os
 import django
 from django.core.management import call_command
 
-# Ajusta 'velerove.settings' si tu archivo de configuración tiene otro nombre principal
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'velero_store.settings')
 django.setup()
 
 with open('catalog/fixtures/productos.json', 'w', encoding='utf-8') as f:
-    call_command('dumpdata', 'catalog.Product', natural_foreign=True, natural_primary=True, indent=4, stdout=f)
+    call_command('dumpdata', 'catalog', natural_foreign=True, natural_primary=True, indent=4, stdout=f)
 
-print("¡Exportado con éxito en UTF-8!")
+print("¡Catálogo completo exportado con éxito en UTF-8!")
